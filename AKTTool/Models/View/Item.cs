b8d5b956001939f663e0eba89705471d1b9251af
Tôn Thất Duy Khang <duykhang91@gmail.com>
2019-03-16 16:48:05 +0700

    fix url and display name

--- AKTTool/Models/View/Item.cs
+++ AKTTool/Models/View/Item.cs
@@ -1,9 +1,16 @@
-﻿namespace AKTTool.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AKTTool.Models
 {
    public class Item : EntityBase
   {
+    [Display(Name = "Type")]
     public string type { get; set; }
+
+    [Display(Name = "Code")]
     public string code { get; set; }
+
+    [Display(Name = "Link")]
     public string link { get; set; }
   }
 }
