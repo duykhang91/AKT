86da2756a508705a13f7f1004385c92522ae9b3c
Tôn Thất Duy Khang <duykhang91@gmail.com>
2019-03-16 13:28:21 +0700

    add less and layout

---
+++ AKT/gulpfile.js
@@ -0,0 +1,10 @@
+﻿/// <binding AfterBuild='less' />
+var gulp = require("gulp"),
+  fs = require("fs"),
+  less = require("gulp-less");
+
+gulp.task("less", function () {
+  return gulp.src('Styles/main.less')
+    .pipe(less())
+    .pipe(gulp.dest('wwwroot/css'));
+});
---
+++ AKT/wwwroot/js/Layout.js
@@ -0,0 +1,9 @@
+﻿$(window).resize(function matchHeight() {
+  windowHeight = $(window).height();
+  document.getElementById('background').setAttribute("style", " min-height:" + windowHeight + "px;");
+});
+
+$(document).ready(function matchHeight() {
+  windowHeight = $(window).height();
+  document.getElementById('background').setAttribute("style", " min-height:" + windowHeight + "px;");
+});
